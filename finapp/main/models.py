238802a98fbb76af8e8785c27a30ca6001ba7d0b
Yegor <EPMironov@stud.kpfu.ru>
2024-07-28 18:43:41 +0300

    Add and migrate main finance models

--- finapp/main/models.py
+++ finapp/main/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from registration.models import CustomUser
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+    type = models.CharField(max_length=30)
+
+
+class Subcategory(models.Model):
+    name = models.CharField(max_length=30)
+
+
+class Income(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    subcategory = models.ForeignKey(Subcategory, on_delete=models.PROTECT)
+    total = models.FloatField()
+    date = models.DateField()
+    comment = models.CharField(max_length=255, null=True)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+
+
+class Expenses(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    subcategory = models.ForeignKey(Subcategory, on_delete=models.PROTECT)
+    total = models.FloatField()
+    date = models.DateField()
+    comment = models.CharField(max_length=255, null=True)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
