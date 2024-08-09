dba1725eaf45e048ba479a4359f320cf762bfb5a
Yegor <EPMironov@stud.kpfu.ru>
2024-08-09 16:30:06 +0300

    update views

--- finapp/registration/views.py
+++ finapp/registration/views.py
@@ -5,20 +5,31 @@ from .models import CustomUser, UserProfile
 import re
 
 
-def is_valid_email(email):
+def is_valid_email(email, type='registration'):
     # Регулярное выражение для проверки формата электронной почты
     email_regex = (r'^[a-zA-Z0-9._%+-]+'
                    r'@'
                    r'[a-zA-Z0-9.-]+'
                    r'\.[a-zA-Z]{2,}$')
-    try:
-        # Если уже есть пользователь с таким email -> False
-        existing_email = CustomUser.objects.get(email=email)
-        return False
-    except Exception as e:
-        if re.match(email_regex, email):
-            return True
-        else:
+    if type == 'registration':
+        try:
+            # Если уже есть пользователь с таким email -> False
+            existing_email = CustomUser.objects.get(email=email)
+            return False
+        except Exception as e:
+            if re.match(email_regex, email):
+                return True
+            else:
+                return False
+    elif type == 'login':
+        try:
+            # Если уже есть пользователь с таким email -> True
+            existing_email = CustomUser.objects.get(email=email)
+            if re.match(email_regex, existing_email):
+                return True
+            else:
+                return False
+        except Exception as e:
             return False
 
 
@@ -90,16 +101,11 @@ def is_valid_date_of_birth(date_of_birth):
     return False
 
 
-import re
-
 def is_valid_phone_number(phone_number):
     # Проверяем, что строка состоит только из цифр и имеет длину 11
     if len(phone_number) == 11 and phone_number.isdigit() and (phone_number.startswith('8') or phone_number.startswith('7')):
         return True
     return False
-
-
-
 
 
 def make_profile(request):
@@ -156,12 +162,21 @@ def login_view(request):
     if request.method == 'POST':
         email = request.POST['email']
         password = request.POST['password']
-        user = authenticate(request, username=email, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('home')
+
+        if is_valid_email(email, type='login') and is_valid_password(password):
+            # Аутентификация пользователя с использованием email и пароля
+            user = authenticate(request, username=email, password=password)
+
+            if user is not None:
+                # Если пользователь найден, выполняем логин
+                login(request, user)
+                return redirect('home')  # Перенаправление на главную страницу или другую
+
+            else:
+                messages.error(request, "Неправильный email или пароль")
         else:
-            return render(request, 'login.html', {'error': 'Invalid email or password'})
+            messages.error(request, "Некорректный email или пароль")
+
     return render(request, 'login.html')
 
 
